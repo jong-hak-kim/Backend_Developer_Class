520cc1fb33f5e9b4bc93dc21e23c8ea984522c45
Jonghak Kim <whdgkr9070@naver.com>
2023-03-29 12:54:41 +0900

    2023.03.29 day24

--- MySQL/Date Type.sql
+++ MySQL/Date Type.sql
@@ -2,10 +2,15 @@
 CREATE DATABASE train_reservation;
 USE train_reservation;
 
+# 실제로 존재하는 것
+# Train, Station, Seat
+
 # 가격 테이블 생성
+
+# Station 테이블에 종속
 CREATE TABLE Cost (
-    departure_station VARCHAR(50) NOT NULL,
-    arrival_station VARCHAR(50) NOT NULL,
+    departure_station INT NOT NULL,
+    arrival_station INT NOT NULL,
     amount INT NOT NULL
 );
 
@@ -22,7 +27,7 @@ CREATE TABLE Train (
 CREATE TABLE Station (
     station_number INT AUTO_INCREMENT PRIMARY KEY,
     station_name VARCHAR(50) NOT NULL,	
-    address TEXT NOT NULL UNIQUE,
+    address VARCHAR(500) NOT NULL UNIQUE,
     tel_number VARCHAR(15) NOT NULL UNIQUE
 );
 
@@ -40,3 +45,31 @@ CREATE TABLE SEAT (
 	train_type VARCHAR(10) NOT NULL #KTX, SRT, 무궁화...
 );
 
+# ALTER로 제약조건 추가 
+ALTER TABLE Cost
+ADD CONSTRAINT cost_primary_key
+PRIMARY KEY (departure_station, arrival_station);
+
+ALTER TABLE Cost
+ADD CONSTRAINT cost_foreign_key_1
+FOREIGN KEY (departure_station)
+REFERENCES Station (station_number);
+
+ALTER TABLE Cost
+ADD cONSTRAINT cost_foreign_key_2
+FOREIGN KEY (arrival_station)
+REFERENCES Station (station_number);
+
+ALTER TABLE STOP_STATION
+ADD CONSTRAINT stop_station_primary_key
+PRIMARY KEY(station_number, train_number);
+
+ALTER TABLE STOP_STATION
+ADD CONSTRAINT stop_station_foreign_key_1
+FOREIGN KEY (station_number)
+REFERENCES Station (station_number);
+
+ALTER TABLE STOP_STATION
+ADD CONSTRAINT stop_station_foreign_key_2
+FOREIGN KEY (train_number)
+REFERENCES Train (train_number);
---
+++ MySQL/기본 정리.sql
@@ -0,0 +1,301 @@
+# 내용 
+-- 띄어쓰기 해야 주석이 됨
+/* 부분 주석 */
+/*
+여러 줄
+주석
+*/
+
+# DDL (데이터 정의어)
+# 데이터베이스 혹은 테이블(뷰, 인덱스, 프로시저...)을 생성, 수정, 삭제 할 때 사용하는 SQL문
+
+# CREATE 문
+# 데이터베이스나, 테이블 등을 생성할 때 사용하는 SQL문
+
+# 데이터베이스 생성
+# CREATE DATABASE 데이터베이스명;
+
+CREATE DATABASE Hotel;
+
+SHOW DATABASES;
+
+# 데이터베이스를 생성했으면 사용할 데이터베이스를 선택
+# USE 데이터베이스명;
+
+USE Hotel;
+
+# 테이블 생성
+# CREATE TABLE 테이블명 (
+#   필드명 데이터타입(데이터길이) 제약조건,
+#   ...
+# );
+
+CREATE TABLE Custom (
+	id INT PRIMARY KEY, -- NOT NULL과 UNIQUE 특징 가짐
+    name VARCHAR(20) NOT NULL,
+    email VARCHAR(30) UNIQUE, -- 중복 방지
+    age INT,
+    address TEXT,
+    accept_marketing BOOLEAN DEFAULT false -- 기본값을 false로 설정한다 (값을 넣지 않더라도 false로 들어가게 된다)
+    -- 필드명에 띄어쓰기가 있으면 띄어쓰기 대신 _ 사용
+);
+
+SHOW TABLES;
+
+# 데이터베이스 수정
+# ALTER DATABASE 데이터베이스명 속성명 = 값;
+
+# 테이블 수정
+# ALTER TABLE 테이블명
+
+# 테이블 필드 추가
+# ALTER TABLE 테이블명 ADD 필드명 데이터타입(데이터길이) 제약조건;
+# 테이블 필드 삭제
+# ALTER TABLE 테이블명 DROP 필드명;
+# 테이블 필드 수정
+# ALTER TABLE 테이블명 MODIFY COLUMN 필드명 데이터타입(데이터길이) 제약조건;
+
+# 데이터베이스 삭제
+# DROP DATABASE 데이터베이스명;
+
+# 테이블 삭제
+# DROP TABLE 테이블명;
+# TRUNCATE TABLE 테이블명;
+
+# DROP TABLE 테이블명 : 테이블의 정의 자체를 제거
+# TRUNCATE TABLE 테이블명 : 테이블을 정의만 남기고 모두 제거 (생성 초기 상태로 돌려놓음)
+
+# DML (데이터 조작어)
+# 데이트베이스 테이블에 레코드를 삽입, 수정, 삭제, 검색 작업을 수행시키고자 할 때 사용하는 SQL문
+
+# 삽입문 (INSERT INTO)
+# 해당 테이블에 레코드를 삽입할 때 사용하는 SQL 구문
+# INSERT INTO 테이블명(필드명1, 필드명2, ...)
+# VALUES (필드1의 데이터, 필드2의 데이터, ...);
+
+INSERT INTO Custom(id, name) VALUES (1, 'John Doe');
+-- INSERT INTO Custom(id, email) VALUES (5, 'qwer@qwer'); 필수 값 미입력
+-- INSERT INTO Custom(id, name, email) VALUES(1, 'John Doe', 'qwer@qwer'); 기본값 중복
+
+SELECT * FROM Custom;
+
+# 모든 필드에 값을 넣을 때:
+# INSERT INTO 테이블명
+# VALUES (데이터1, 데이터2, ...);
+
+INSERT INTO Custom VALUES (10, 'John Doe', 'rewq1@qwer.com', 30, 'This is Address', true);
+INSERT INTO Custom VALUES (11, 'Michle', 'rewq2@qwer.com', 30, 'This is Address', true);
+INSERT INTO Custom VALUES (12, 'Michle', 'rewq3@qwer.com', 30, 'This is Address', true);
+
+INSERT INTO Custom
+VALUES (11, 'Jonghak', 'whdgkr9070@naver.com', 31, 'Busan');
+
+# 수정문 (UPDATE)
+# 해당 테이블에서 특정 필드의 값을 수정하고자 할 때
+# 사용하는 SQL문
+
+# UPDATE 테이블명 SET 필드명1 = 데이터1, 필드명2 = 데이터2, ...;
+UPDATE Custom SET accept_marketing = false;
+UPDATE Custom SET accept_marketing = true, age = 10;
+
+# UPDATE Custom SET 필드명1 = 데이터1, ...
+# WHERE 조건;
+UPDATE Custom SET email ='email@gamil.com'
+WHERE id = 1;
+
+# 삭제문 (DELETE)
+# 해당 테이블에서 레코드를 삭제할 때 사용하는 SQL문
+
+# DELETE FROM 테이블명;
+DELETE FROM Custom;
+
+# DELETE FROM 테이블명 WHERE 조건;
+DELETE FROM Custom WHERE name = 'John Doe';
+
+# 검색문 (SELECT)
+# 해당 테이블에서 특정 필드를 검색하고자 할 떄 사용하는 SQL문
+
+# SELECT 필드명1, 필드명2, ... FROM 테이블명;
+SELECT id, name FROM Custom;
+
+# SELECT * FROM 테이블명;
+SELECT * FROM Custom;
+
+# 필드에 별칭 사용
+SELECT name AS 'Full Name' FROM Custom;
+
+# 테이블에 별칭 사용
+SELECT name FROM Custom AS C;
+
+# 중복 제거 (DISTINCT)
+SELECT DISTINCT name FROM Custom;
+
+# 연산자
+# 비교 연산
+
+# BETWEEN a AND b
+# a 보다 크거나 같으면서 b 보다 작거나 같은 값을 찾음
+SELECT * FROM Custom WHERE age BETWEEN 10 AND 20;
+
+# IN()
+# 인수로 전달된 값과 동일한 값이 하나라도 존재한다면 true를 반환
+SELECT * FROM Custom
+WHERE name IN ('John Doe', 'Michle', 'James'); # 이름이 'John Doe', 'Michle', 'James' 인 애를 출력
+
+SELECT * FROM Custom
+WHERE name = 'John Doe'
+OR name = 'Michle'
+OR name = 'Jamaes'; # IN과 동일
+
+# IS
+# 비교 대상이 Boolean 형태일 때 사용하는 비교 연산자
+SELECT * FROM Custom
+WHERE accept_marketing IS true;
+
+# IS NULL
+# 비교 대상이 Null이면 true를 반환
+SELECT * FROM Custom
+WHERE email IS NULL;
+
+# LIKE
+# 문자열의 패턴을 비교하여 동일한 패턴을 가지고 있는 문자열이면 true를 반환
+
+# 와일드 카드
+# % : 0개 이상의 패턴
+# _ : 1개의 패턴
+SELECT * FROM Custom
+WHERE email LIKE '%gmail%';
+
+# Constraint (제약조건)
+# RDBMS에서 삽입, 수정, 삭제에 대해서 무결성을 보장해주는 조건
+
+# NOT NULL
+# 입력 혹은 수정 작업에 있어서 해당 필드에 Null이 올 수 없도록 하는
+# 제약 조건
+
+# Create
+CREATE TABLE NotnullTable1 (
+	notnull_field INT NOT NULL
+);
+
+CREATE TABLE NotnullTable (
+	notnull_field INT
+);
+
+
+# Alter
+# Alter로 NOT NULL 제약 조건을 추가할 땐
+# 원래 존재하는 레코드에서 해당 필드의 데이터가 Null이 존재하면 안됨
+ALTER TABLE NotnullTable1
+MODIFY COLUMN notnull_field INT NOT NULL;
+
+# Default
+# 입력 작업에서 해당 필드의 값이 들어오지 않으면 기본값으로 지정해주는 제약 조건
+# Default로 값이 들어오는 필드는 NOT NULL 입력하지 않아도 됨
+
+## CREATE
+CREATE TABLE defaultTable1 (
+	default_field INT DEFAULT 1
+);
+
+## ALTER
+CREATE TABLE defaultTable2 (
+	default_field INT
+);
+
+ALTER TABLE defaultTable2
+MODIFY COLUMN default_field INT DEFAULT 1;
+
+# UNIQUE
+# 삽입, 수정 작업에서 해당 제약 조건이 걸려있는 필드의 데이터에 대해
+# 중복을 허용하지 않는 제약 조건
+
+## CREATE
+CREATE TABLE Unique_Table1 (
+	unique_field INT UNIQUE
+);
+
+CREATE TABLE Unique_Table2 (
+	unique_field INT,
+    CONSTRAINT unique_key_1 #제약 조건에 이름을 붙여주는 행위 (생략 가능)
+    UNIQUE (unique_field)
+);
+
+## ALTER
+CREATE TABLE Unique_Table3 (
+	unique_field INT
+);
+
+ALTER TABLE Unique_Table3
+MODIFY COLUMN unique_field INT UNIQUE;
+
+CREATE TABLE Unique_Table4 (
+	unique_field INT
+);
+
+ALTER TABLE Unique_Table4
+ADD CONSTRAINT unique_key_1
+UNIQUE (unique_field);
+
+# PRIMARY KEY
+# 기본키에 대한 제약 조건, NOT NULL / UNIQUE가 포함되어 있음
+# 삽입, 수정 시에 NULL을 포함할 수 없음, 중복된 데이터를 포함할 수 없음
+
+## CREATE
+CREATE TABLE primary_table1 (
+	primary_field INT PRIMARY KEY
+);
+
+CREATE TABLE primary_table2 (
+	primary_field INT,
+    CONSTRAINT primary_key_1
+    PRIMARY KEY (primary_field)
+);
+
+## ALTER
+CREATE TABLE primary_table3 (
+	primary_field INT
+);
+
+ALTER TABLE primary_table3
+MODIFY COLUMN primary_field INT PRIMARY KEY;
+
+CREATE TABLE primary_table4 (
+	primary_field INT
+);
+
+ALTER TABLE primary_table4
+ADD CONSTRAINT primary_key_1 PRIMARY KEY (primary_field);
+
+# FOREIGN KEY
+# 참조 제약조건, 해당 테이블을 해당 필드를 기준으로
+# 외부 테이블의 외부 필드를 참조하도록 하는 제약 조건
+# 해당 제약 조건이 걸려있는 필드의 경우 참조하는 테이블의 참조 필드에 존재하는 데이터만 삽입할 수 있음
+
+## CREATE
+CREATE TABLE Referenced_Table (
+	primary_key INT PRIMARY KEY
+);
+
+# CREATE 싣에 참조 제약 조건을 추가할 땐
+# 선행적으로 참조할 테이블과 필드가 존재해야하고
+# 참조할 필드가 PRIMARY KEY 혹은 UNIQUE 제약 조건이 지정되어 있어야 한다
+# 참조 제약조건이 걸리는 필드는 참조할 필드의 데이터타입과 일치해야 함
+CREATE TABLE Foreign_Table1 (
+	foreign_field INT,
+    CONSTRAINT foreign_key
+    FOREIGN KEY (foreign_field)
+    REFERENCES Referenced_Table
+);
+
+# ALTER
+CREATE TABLE Foreign_Table2 (
+	foreign_field INT
+);
+
+ALTER TABLE Foreign_Table2
+ADD CONSTRAINT foreign_key_1
+FOREIGN KEY (foreign_field)
+REFERENCES Referenced_Table (primary_key)
+
+
